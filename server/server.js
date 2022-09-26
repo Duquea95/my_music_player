592cb4be41e119c7eb3701c3e3ea077f42602339
Anthony <duquea95@gmail.com>
2022-09-26 16:06:12 -0400

    Dashboard-ContextMenu

---
+++ server/server.js
@@ -0,0 +1,59 @@
+// require("dotenv").config()
+const express = require("express")
+const cors = require("cors")
+const bodyParser = require("body-parser")
+// const lyricsFinder = require("lyrics-finder")
+const SpotifyWebApi = require("spotify-web-api-node")
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.post("/login", (req, res) => {
+    let code = req.body.code
+
+
+    let spotifyApi = new SpotifyWebApi({
+        redirectUri: "http://localhost:3000",
+        clientId: '394758341b2147028b2ec9f669d38ba0',
+        clientSecret: 'a6c64a39f405434c8b3f4a219a048a94'
+    })
+
+    spotifyApi.authorizationCodeGrant(code)
+        .then(data => {
+        res.json({
+            accessToken: data.body.access_token,
+            refreshToken: data.body.refresh_token,
+            expiresIn: data.body.expires_in,
+        })
+        console.log(res)
+        }).catch(err => {
+            console.log("Broken"+err)
+        })
+})
+
+app.post("/refresh", (req,res) => {
+    const refreshToken = req.body.refreshToken
+    const spotifyApi = new SpotifyWebApi({
+        clientId: '394758341b2147028b2ec9f669d38ba0',
+        clientSecret: 'a6c64a39f405434c8b3f4a219a048a94',
+        redirectUri: "http://localhost:3000",
+        refreshToken,
+    })
+    
+    spotifyApi
+        .refreshAccessToken()
+        .then(data => {
+            res.json({
+                accessToken: data.body.access_token,
+                expiresIn: data.body.expires_in,
+            })
+        }).catch(err => {console.log(err)})
+})
+
+// app.get("/search",(req,res) => {
+//     console.log("Searched")
+// })
+
+app.listen(3001)
